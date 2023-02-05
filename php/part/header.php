cf47c25ba7f052dce93439c479b7d992e308633d
Enzobu <enzo.palermo@epsi.fr>
2023-02-05 10:32:55 +0100

    pb nav responsive

--- php/part/header.php
+++ php/part/header.php
@@ -17,3 +17,17 @@
         <li class="nav-item"><a href="mail"><p>Test mail</p></a></li>
     </ul>
 </nav>
+<nav class="navR" id="nav">
+    <ul class="nav-list">
+        <li class="nav-item" onclick="changerLeft2('-18rem');"><a href="#menus">Menus</a></li>
+        <li class="nav-item" onclick="changerLeft2('-18rem');"><a href="#pourquoi-hi-mix">Hi-Mix ?</a></li>
+    </ul>
+    <ul class="nav-list">
+        <li class="nav-item"><a href="">HI-MIX</a></li>
+    </ul>
+    <ul class="nav-list">
+        <li class="nav-item"><a href="account"><img src="img/compte.svg" alt="mon compte"><p class="resonsive">Mon coermpte</p></a></li>
+        <li class="nav-item"><a href="cart"><img src="img/panier.svg" alt="panier"><p class="resonsive">Panier</p></a></li>
+        <li class="nav-item"><a href="mail"><p>Test mail</p></a></li>
+    </ul>
+</nav>
