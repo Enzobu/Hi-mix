9612f264272d6b35bb1c2517e4caab072c954724
Enzobu <enzo.palermo@epsi.fr>
2023-03-27 13:39:38 +0200

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('php/head.php'); ?>
+    <link rel="stylesheet" href="css/404/404.css">
+    <title>Panier - Hi-Mix</title>
+</head>
+<body id="body">
+    <header>
+        <div class="header">
+            <?php  include("php/header.php"); ?>
+            <div class="title">
+                <h1><strong>HI-MIX</strong></h1>
+                <h2>La page demandé n'existe pas.</h2>
+                <a href="/">RETOUR A L'ACCUEIL</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="scrollUp">
+        <a href="#top"><img src="img/up-arrow.png"/></a>
+    </div>
+
+    <footer>
+        <?php include("php/footer.php"); ?>
+    </footer>
+</body>
+</html>
