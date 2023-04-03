8fe11f9f42e625aa136c3d0a5920b053461ed2d5
Enzobu <enzo.palermo@epsi.fr>
2023-04-03 15:51:45 +0200

    add index of admin

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php include('php/isAdmin.php'); ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('php/head_admin.php'); ?>
+        <link rel="stylesheet"  href="/admin/style/style_index.css">
+    </head>
+
+    <body>
+        <header>
+            <?php include('php/header_admin.php'); ?>
+        </header>
+
+        <div class="global-container">
+            <p> AUCUNE TABLE SÉLECTIONNÉE(S). </p>
+            <p> POUR CONTINUER, VEUILLEZ SÉLECTIONNER UNE TABLE CI-DESSUS. </p>
+        </div>
+    </body>
+</html>
