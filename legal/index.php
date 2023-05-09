88cfd75bb44d6562928aec98f0e5c6eb62ae4f7e
Enzobu <enzo.palermo@epsi.fr>
2023-05-09 15:07:10 +0200

    Create index.php

---
+++ legal/index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('../php/head_menu.php'); ?>
+    <link rel="stylesheet" href="../css/legal/legal.css">
+    <title>Menu simple - Hi-Mix</title>
+</head>
+<body id="body">
+    <header>
+        <div class="header">
+            <?php  include("../php/header_menu.php"); ?>
+            <div class="title">
+                <h1><strong>HI-MIX</strong></h1>
+                <h2>Léglité - RGPD</h2>
+            </div>
+        </div>
+    </header>
+
+    <div class="bg-body-1" id="CGV">
+        <h2>Condition Générale de Vente (CGV) :</h2>
+        <p>
+            <div class="article">
+                <div class="titre">
+                    1. Champ d'application :
+                </div>
+                Les présentes conditions générales de vente s'appliquent à toutes les commandes passées sur notre site de vente de nourriture en ligne.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    2. Produits proposés :
+                </div>
+                Nous proposons à la vente sur notre site de la nourriture destinée à la consommation humaine. Les produits proposés sont décrits avec 
+                précision sur les fiches produits disponibles sur notre site.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    3. Commande :
+                </div>
+                Pour passer une commande sur notre site, vous devez sélectionner les produits que vous souhaitez acheter et les ajouter à votre panier. 
+                Une fois vos achats terminés, vous devez valider votre panier et procéder au paiement.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    4. Prix et paiement :
+                </div>
+                Les prix de nos produits sont indiqués en euros, toutes taxes comprises. Le paiement s'effectue en ligne, au moment de la validation de 
+                la commande. Nous acceptons les cartes bancaires et les paiements PayPal.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    5. Livraison :
+                </div>
+                Nous assurons la livraison des produits à l'adresse que vous avez indiquée lors de votre commande. Les délais de livraison peuvent varier 
+                en fonction de votre lieu de résidence et du volume de commandes que nous avons à traiter. Nous vous informerons du suivi de votre commande par e-mail.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    6. Réclamation et retour de produits :
+                </div>
+                Si vous constatez un problème avec les produits que vous avez commandés, vous pouvez nous en informer par e-mail dans les plus brefs délais. 
+                Nous vous proposerons alors une solution de remplacement ou de remboursement.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    7. Données personnelles :
+                </div>
+                Nous nous engageons à ne pas divulguer les données personnelles que vous nous avez communiquées lors de votre commande à des tiers. Nous 
+                utilisons ces données uniquement pour la gestion de votre commande et pour vous informer de l'état de votre commande.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    8. Propriété intellectuelle :
+                </div>
+                Tous les éléments présents sur notre site (textes, images, logos, etc.) sont protégés par le droit de la propriété intellectuelle. Toute 
+                reproduction, totale ou partielle, de ces éléments est strictement interdite.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    9. Modification des conditions générales de vente :
+                </div>
+                Nous nous réservons le droit de modifier à tout moment les présentes conditions générales de vente. Les conditions applicables seront 
+                celles en vigueur au moment de la passation de votre commande.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    10. Droit applicable et juridiction compétente :
+                </div>
+                Les présentes conditions générales de vente sont régies par le droit français. En cas de litige, les tribunaux français seront seuls compétents.
+            </div>
+        </p>
+    </div>
+
+    <div class="bg-body-2" id="CGU">
+        <h2>Condition Générale d'Utilisation (CGU) :</h2>
+        <p>
+            <div class="article">
+                <div class="titre">
+                    1. Objet :
+                </div>
+                Les présentes conditions générales d'utilisation ont pour objet de définir les modalités d'accès et d'utilisation du site de vente de 
+                nourriture en ligne.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    2. Acceptation des conditions :
+                </div>
+                L'utilisation du site implique l'acceptation pleine et entière des présentes conditions générales d'utilisation. Si vous n'acceptez pas 
+                ces conditions, vous devez cesser toute utilisation du site.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    3. Accès au site :
+                </div>
+                L'accès au site est réservé aux personnes majeures et capables juridiquement. L'accès peut être suspendu à tout moment pour des raisons 
+                de maintenance, de sécurité ou pour tout autre motif.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    4. Utilisation du site :
+                </div>
+                L'utilisation du site doit se faire dans le respect des lois et règlements en vigueur. Il est interdit d'utiliser le site à des fins 
+                illégales ou frauduleuses. Le site ne peut être utilisé à des fins commerciales sans notre accord préalable.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    5. Données personnelles :
+                </div>
+                Nous collectons des données personnelles pour permettre le bon fonctionnement du site et la gestion des commandes. Vous disposez d'un droit 
+                d'accès, de rectification et de suppression de vos données personnelles. Pour plus d'informations, veuillez consulter notre politique de confidentialité.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    6. Propriété intellectuelle :
+                </div>
+                Tous les éléments présents sur le site (textes, images, logos, etc.) sont protégés par le droit de la propriété intellectuelle. Toute 
+                reproduction, totale ou partielle, de ces éléments est strictement interdite.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    7. Responsabilité :
+                </div>
+                Nous ne pouvons garantir l'exactitude, l'exhaustivité ou l'actualité des informations présentes sur le site. Nous ne saurions être tenus 
+                responsables de tout dommage direct ou indirect résultant de l'utilisation du site.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    8. Modification des conditions générales d'utilisation :
+                </div>
+                Nous nous réservons le droit de modifier à tout moment les présentes conditions générales d'utilisation. Les conditions applicables seront 
+                celles en vigueur au moment de l'utilisation du site.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    9. Loi applicable et juridiction compétente :
+                </div>
+                Les présentes conditions générales d'utilisation sont régies par le droit français. En cas de litige, les tribunaux français seront seuls 
+                compétents.
+            </div>
+        </p>
+    </div>
+
+    <div class="bg-body-3" id="politiqur-de-confidentialite">
+        <h2>Politique de Confidentialité :</h2>
+        <p>
+            <div class="article">
+                <div class="titre">
+                    1. Introduction :
+                </div>
+                Nous attachons une grande importance à la protection de la vie privée de nos clients. La présente politique de confidentialité explique 
+                comment nous collectons, utilisons et protégeons vos données personnelles lorsque vous utilisez notre site de vente de nourriture en ligne.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    2. Collecte des données personnelles :
+                </div>
+                Nous collectons les données personnelles que vous nous fournissez lorsque vous passez une commande sur notre site, lorsque vous créez un 
+                compte client, lorsque vous vous abonnez à notre newsletter ou lorsque vous nous contactez. Les données collectées peuvent inclure votre 
+                nom, votre adresse e-mail, votre adresse postale, votre numéro de téléphone, ainsi que les détails de votre commande.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    3. Utilisation des données personnelles :
+                </div>
+                Nous utilisons les données personnelles que nous collectons pour les finalités suivantes :
+            </div>
+            <div class="article">
+                <div class="titre">
+                    4. Pour traiter et livrer votre commande :
+                </div>
+                Pour vous contacter en cas de besoin concernant votre commande
+                <br>
+                Pour gérer votre compte client
+                <br>
+                Pour vous envoyer des newsletters et des offres promotionnelles (avec votre consentement préalable)
+                <br>
+                Pour améliorer notre site et nos services
+                <br>
+                Protection des données personnelles
+                <br>
+                Nous prenons des mesures techniques et organisationnelles appropriées pour protéger vos données personnelles contre la perte, le vol, 
+                l'utilisation abusive, la divulgation non autorisée ou la modification. Nous ne communiquons pas vos données personnelles à des tiers 
+                à des fins commerciales.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    5. Cookies :
+                </div>
+                Notre site utilise des cookies pour améliorer votre expérience utilisateur et pour nous aider à comprendre comment vous utilisez notre 
+                site. Vous pouvez configurer votre navigateur pour refuser les cookies, mais cela peut affecter la fonctionnalité de certaines parties du site.
+            </div>
+            <div class="article">
+                <div class="titre">
+                6. Droits des utilisateurs :
+                </div>
+                Vous disposez d'un droit d'accès, de rectification, d'effacement, de limitation, de portabilité et d'opposition au traitement de vos 
+                données personnelles. Pour exercer ces droits, veuillez nous contacter à l'adresse e-mail indiquée sur notre site.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    7. Durée de conservation des données personnelles :
+                </div>
+                Nous conservons vos données personnelles uniquement pendant la durée nécessaire pour remplir les finalités pour lesquelles elles ont 
+                été collectées, sauf si une durée de conservation plus longue est nécessaire en vertu de la loi.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    8. Modification de la politique de confidentialité :
+                </div>
+                Nous nous réservons le droit de modifier à tout moment la présente politique de confidentialité. Les modifications seront publiées 
+                sur notre site. En continuant à utiliser notre site, vous acceptez les modifications apportées à la politique de confidentialité.
+            </div>
+            <div class="article">
+                <div class="titre">
+                    9. Contact :
+                </div>
+                Si vous avez des questions concernant notre politique de confidentialité ou si vous souhaitez exercer vos droits, veuillez nous 
+                contacter à l'adresse e-mail indiquée sur notre site.
+            </div>
+        </p>
+    </div>
+
+    <div class="bg-body-4"></div>
+
+    <div class="scrollUp">
+        <a href="menu-simple#top"><img src="../img/up-arrow.svg"/></a>
+    </div>
+
+    <footer>
+        <?php include("../php/footer_menu.php"); ?>
+    </footer>
+    
+</body>
+</html>
